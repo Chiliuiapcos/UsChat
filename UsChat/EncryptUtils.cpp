565dc9b8c8882215922988f4b35b5d2e0410ec0f
Chiliuiapcos <754533873@qq.com>
2020-10-03 11:17:10 +0800

    short LAP func

--- UsChat/EncryptUtils.cpp
+++ UsChat/EncryptUtils.cpp
@@ -13,15 +13,11 @@ QString EncryptUtils::Base64ToQString(QString base64) {
 }
 
 QString EncryptUtils::UAL_QStringToBase64(QString src) {
-	QByteArray text = src.toLocal8Bit();
-	QByteArray by = text.toBase64();
-	QString str = QStringUtils::toUpperAndLower(QString::fromLocal8Bit(by));
+	QString str = QStringUtils::toUpperAndLower(QStringToBase64(src));
 	return str;
 }
 
 QString EncryptUtils::UAL_Base64ToQString(QString base64) {
-	QString str = QStringUtils::toUpperAndLower(base64);
-	QByteArray text = base64.toLocal8Bit();
-	QByteArray by = QByteArray::fromBase64(text);
-	return QString::fromLocal8Bit(by);
+	QString str = QStringUtils::toUpperAndLower(base64.toLocal8Bit());
+	return Base64ToQString(str);
 }
